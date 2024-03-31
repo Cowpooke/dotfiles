f43970d93c30b012a90f99cc6c9e239dc56a7797
Cowpooke <massterx9@gmail.com>
2024-03-31 10:20:34 +0700

    add home manager to flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,17 +2,27 @@
   description = "Cowpooke flake";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/release-23.11;
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs,home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [./configuration.nix];
+      };
+    };
+    homeConfigurations = {
+      hilmy = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [./home.nix];
       };
     };
   };
