b3a147d45a3b728ed299a7fe0be422534d5fc83e
Cowpooke <massterx9@gmail.com>
2024-03-31 12:57:18 +0700

    edit home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,14 +1,26 @@
 { config, pkgs, ... }:
 
-imports = [
-  ./packages/packages.nix
-];
-
 {
   home.username = "hilmy";
   home.homeDirectory = "/home/hilmy";
 
   home.stateVersion = "23.11";
+
+  home.packages = with pkgs [
+    hello
+    firefox
+    wget  
+    vscodium  
+    neovim
+    git
+    rofi
+    discord
+    alacritty
+    fish
+    steam
+    corefonts
+    nerdfonts
+  ];
 
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
